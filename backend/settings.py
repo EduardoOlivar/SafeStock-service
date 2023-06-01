7fcd55cdc14515ddf7cf44d80f70b6c0e81e2286
Jean Pierre Rodríguez <jprodriguez@silta.cl>
2023-05-31 20:05:53 -0400

    fix: email desde config

--- backend/settings.py
+++ backend/settings.py
@@ -187,6 +187,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'safestock.app@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = True

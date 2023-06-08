407e199204880f8d3b4f66c0f891e8ef6216722f
Eduardo <jose.olivar@safetyclub.cl>
2023-06-07 20:51:27 -0400

    fix profile view

--- api/serializers.py
+++ api/serializers.py
@@ -78,8 +78,13 @@ class ProfileUserSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance: Users): #metodo para poder agregar data de otras tablas haciendo joins entre tablas
         data = super().to_representation(instance)
-        data['name_shop'] = instance.shop.name
-        data['address_shop'] = instance.shop.address
+        if hasattr(instance, 'shop'):
+            data['name_shop'] = instance.shop.name
+            data['address_shop'] = instance.shop.address
+        else:
+            data['name_shop'] = None
+            data['address_shop'] = None
+
         return data
 
 

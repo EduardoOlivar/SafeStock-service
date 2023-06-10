e44676e609ffed5172eed309ac237cceb52f0645
Eduardo <jose.olivar@safetyclub.cl>
2023-06-10 18:57:14 -0400

    fix url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 
 
 urlpatterns = [
-    re_path(r'^report_items/(?P<shop_id>[0-9]+)/$', views.ReportShopAllItemView.as_view()),
-    re_path(r'^report_items/(?P<shop_id>[0-9]+)/$', views.ReportShopItemView.as_view()),
+    re_path(r'^report_all_items/(?P<shop_id>[0-9]+)/$', views.ReportShopAllItemView.as_view()),
+    re_path(r'^report_item/(?P<shop_id>[0-9]+)/$', views.ReportShopItemView.as_view()),
 ]

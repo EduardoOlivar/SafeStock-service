b721e9d5a83cb9f17ca17e68f6ecae505c408afd
Eduardo <jose.olivar@safetyclub.cl>
2023-06-10 18:33:52 -0400

    add report from shop to all item in a day

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
@@ -23,6 +24,51 @@ class GenericDashBoard(APIView):
 
     def timestamp_to_datetime(self,timestamp):
         return datetime.fromtimestamp(timestamp)
+
+
+class ReportShopAllItemView(GenericDashBoard):
+
+    def get(self, request, *args, **kwargs):
+        shop = self.get_validate_shop(request, *args, return_response=True, **kwargs)
+        if isinstance(shop, Response):
+            return shop
+        start_timestamp = int(request.GET.get('start'))
+        end_timestamp = int(request.GET.get('end'))
+
+        start_datetime = self.timestamp_to_datetime(start_timestamp)
+        end_datetime = self.timestamp_to_datetime(end_timestamp)
+
+        response_data = self.process_report(shop, start_datetime, end_datetime)
+
+        return Response(response_data)
+
+    def process_report(self, shop, start_datetime, end_datetime):
+        days_difference = (end_datetime - start_datetime).days + 1
+
+        report_data = []
+
+        for i in range(days_difference):
+            current_date = start_datetime + timedelta(days=i)
+            report_entry = {}
+
+            #consulta para obtener los datos de venta por dia
+            shop_items = ShopItems.objects.filter(shop_id=shop.id, date__date=current_date.date())
+
+            if shop_items:
+                #se guarda los datos si existe el item en la shop para el dia
+                total_sold = shop_items.aggregate(total_sold=Sum('total_sold'))['total_sold']
+            else:
+                #si no hay datos de venta para el día, se le asigna 0
+                total_sold = 0
+
+            #construcción del objeto de entrada para el informe por dia
+            report_entry['date'] = current_date.timestamp()
+            report_entry['total_sold'] = total_sold
+
+            report_data.append(report_entry)
+
+        return report_data
+
 
 
 class ReportShopItemView(GenericDashBoard):
@@ -70,7 +116,8 @@ class ReportShopItemView(GenericDashBoard):
                 weight_sold = 0
 
             #construcción del objeto de entrada para el informe por dia
-            report_entry[current_date.date()] = {
+            current_date_timestamp = current_date.timestamp()
+            report_entry[current_date_timestamp] = {
                 'sold': total_sold,
                 'weight': weight_sold,
                 'quantity': quantity_sold
